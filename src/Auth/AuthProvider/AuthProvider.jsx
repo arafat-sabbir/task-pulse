ea2a536ca12b6a4664bbeb384b20ff3767b9532c
arafat-sabbir <arafatshabbir8@gmail.com>
2024-01-19 14:04:38 +0600

    Added Sign Function And Context APi For Sharing Relative Functions

--- src/Auth/AuthProvider/AuthProvider.jsx
+++ src/Auth/AuthProvider/AuthProvider.jsx
@@ -14,50 +14,58 @@ import app from "../FireBase/firebase.config";
 export const Context = createContext("");
 
 const AuthProvider = ({ children }) => {
+  // Store The Currently SignIn User Data
   const [user, setUser] = useState("");
+  //   Loading State For Managing Loading State
   const [loader, setLoader] = useState(true);
   const auth = getAuth(app);
-
+  // SingUp User With Email And Password
   const signUpUser = (email, password) => {
     setLoader(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
-contextValue
-const updateUserProfile = (name, photoUrl) => {
-  setLoader(true);
-  return updateProfile(auth.currentUser, {
-    displayName: name,
-    photoURL: photoUrl,
-  });
-};
-const signInUser = (email, password) => {
-  setLoader(true);
-  return signInWithEmailAndPassword(auth, email, password);
-};
-const signOutUser = () => {
-  return signOut(auth);
-};
 
-const contextValue = {
-  signUpUser,
-  signInUser,
-  user,
-  loader,
-  signOutUser,
-  updateUserProfile,
-};
-useEffect(() => {
-  const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-    setUser(currentUser);
-    setLoader(false)
-  });
-  return () => unsubscribe();
-}, [auth]);
+  //   Update Profile Function For Updating User Profile
+  const updateUserProfile = (name, photoUrl) => {
+    setLoader(true);
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photoUrl,
+    });
+  };
+
+  // Sign In User With Email And Password Function
+  const signInUser = (email, password) => {
+    setLoader(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  // Sign Out User Function
+  const signOutUser = () => {
+    return signOut(auth);
+  };
+  // Share All The Relative Function With Context Api As Context Value
+  const contextValue = {
+    signUpUser,
+    signInUser,
+    user,
+    loader,
+    signOutUser,
+    updateUserProfile,
+  };
+
+  // Oversee The User If SignIn Or Not
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoader(false);
+    });
+    return () => unsubscribe();
+  }, [auth]);
   return <Context.Provider value={contextValue}>{children}</Context.Provider>;
 };
 
 export default AuthProvider;
-
+// PropTypes For The Children
 AuthProvider.propTypes = {
   children: PropTypes.node,
 };
